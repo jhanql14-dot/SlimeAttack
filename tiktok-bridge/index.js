b9600f7355f088a229aaa01e9c960a4f69608924
ronalth26 <117491977+ronalth26@users.noreply.github.com>
2026-05-01 11:25:22 -0500

    as

--- tiktok-bridge/index.js
+++ tiktok-bridge/index.js
@@ -11,7 +11,7 @@ wss.on('connection', (ws) => {
 });
 
 // Conectar a TikTok (reemplaza con tu usuario cuando estés en vivo)
-let tiktokUsername = "mrwalterpe";
+let tiktokUsername = "waniwaney";
 let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
 
 tiktokLiveConnection.connect().then(state => {
